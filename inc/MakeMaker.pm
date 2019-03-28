135d2bfdc84f5f6e0392ee865ed0ba61257693d1
Jacques Germishuys <jacques.germishuys@gmail.com>
2019-03-28 12:08:11 +0000

    Only use TCP_KEEPIDLE on Linux

--- inc/MakeMaker.pm
+++ inc/MakeMaker.pm
@@ -133,7 +133,7 @@ if ($is_linux || $is_osx || ($is_bsd && !$is_gkfreebsd && !$is_openbsd))
 		'ZMQ_HAVE_TCP_KEEPALIVE';
 }
 
-if ($is_linux || $is_osx || ($is_bsd && !$is_gkfreebsd && !$is_openbsd))
+if ($is_linux)
 {
 	push @opts,
 		'ZMQ_HAVE_TCP_KEEPIDLE',
